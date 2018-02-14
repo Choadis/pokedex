631fda7c2a3fd1bdd1139895360bad42b45c5517
Choadis <dahlgrensm@gmail.com>
2018-02-14 14:58:38 -0500

    Reinitialized repo after psql issues

---
+++ test/controllers/pokemons_controller_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class PokemonsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @pokemon = pokemons(:one)
+  end
+
+  test "should get index" do
+    get pokemons_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_pokemon_url
+    assert_response :success
+  end
+
+  test "should create pokemon" do
+    assert_difference('Pokemon.count') do
+      post pokemons_url, params: { pokemon: { ability_1: @pokemon.ability_1, ability_2: @pokemon.ability_2, hidden_ability: @pokemon.hidden_ability, name: @pokemon.name, type1: @pokemon.type1, type2: @pokemon.type2 } }
+    end
+
+    assert_redirected_to pokemon_url(Pokemon.last)
+  end
+
+  test "should show pokemon" do
+    get pokemon_url(@pokemon)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_pokemon_url(@pokemon)
+    assert_response :success
+  end
+
+  test "should update pokemon" do
+    patch pokemon_url(@pokemon), params: { pokemon: { ability_1: @pokemon.ability_1, ability_2: @pokemon.ability_2, hidden_ability: @pokemon.hidden_ability, name: @pokemon.name, type1: @pokemon.type1, type2: @pokemon.type2 } }
+    assert_redirected_to pokemon_url(@pokemon)
+  end
+
+  test "should destroy pokemon" do
+    assert_difference('Pokemon.count', -1) do
+      delete pokemon_url(@pokemon)
+    end
+
+    assert_redirected_to pokemons_url
+  end
+end
