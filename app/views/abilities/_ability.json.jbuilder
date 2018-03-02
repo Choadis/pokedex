a1c2cbb7d88cf5b8910813dd13ae7c6bd5d0f83b
Choadis <dahlgrensm@gmail.com>
2018-03-02 14:47:09 -0500

    Blank slate...

--- app/views/abilities/_ability.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! ability, :id, :name, :created_at, :updated_at
-json.url ability_url(ability, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228191033) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "abilities", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "pokemons", force: :cascade do |t|
-    t.string "name"
-    t.string "type1"
-    t.string "type2"
-    t.string "ability_1"
-    t.string "ability_2"
-    t.string "hidden_ability"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "types", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
