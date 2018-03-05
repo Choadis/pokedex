2ddcfb991766f24880e628b9fd14ac5fecd7017f
Choadis <dahlgrensm@gmail.com>
2018-03-05 00:14:20 -0500

    Got sign up ready with secure password

--- Gemfile
+++ Gemfile
@@ -4,7 +4,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-gem 'rails',        '5.1.4'
+gem 'rails',          '5.1.4'
+gem 'bcrypt',         '3.1.11'
 gem 'puma',         '3.9.1'
 gem 'sass-rails',   '5.0.6'
 gem 'uglifier',     '3.2.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302201130) do
+ActiveRecord::Schema.define(version: 20180305050214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 20180302201130) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "pokemon_abilities", "abilities"
